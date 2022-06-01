6e946aaabf3b6fabd56851f7ae5df415305a9ea4
Tej Patel <tejpatel317@gmail.com>
2022-06-01 16:31:15 -0400

    commit tree 68bb1de008454db7943bb39a6ed73c8fc920788f

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    
+    if (timeRemaining === 0) {
+      setTimeRemaining(10)
+      onAnswered(false)
+      return
+    }
+    
+    const timerID = setTimeout(() => setTimeRemaining(timeRemaining - 1),1000)
+    
+    return function () {
+      clearTimeout(timerID)
+    }
+    
+  }, [timeRemaining])
+  
+  /*if (timeRemaining === 0) {
+    setTimeRemaining(10)
+    onAnswered(false)
+  }*/
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
